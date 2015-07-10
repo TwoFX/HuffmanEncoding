ac06a8b2a518813509a815a14562fd2887c14fa0
Markus Himmel <markus@himmel-villmar.de>
2015-07-10 23:58:26 +0200

    Priority Queues implemented with some tests

---
+++ HuffmanEncoding/Classes/PriorityQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoding
+{
+    class PriorityQueue<T>
+    {
+        private class PriorityComparer<TPayload> : IComparer<Tuple<TPayload, int>>
+        {
+            private static IComparer<int> comp = Comparer<int>.Default;
+
+            public int Compare(Tuple<TPayload, int> first, Tuple<TPayload, int> second)
+            {
+                return comp.Compare(first.Item2, second.Item2);
+            }
+        }
+
+        private BinaryHeap<Tuple<T, int>> underlying;
+
+        public PriorityQueue()
+            : this(Enumerable.Empty<Tuple<T, int>>())
+        {
+        }
+
+        public PriorityQueue(IEnumerable<Tuple<T, int>> source)
+        {
+            underlying = new BinaryHeap<Tuple<T, int>>(source, new PriorityComparer<T>());
+        }
+
+        public void Push(T value, int priority)
+        {
+            underlying.Push(new Tuple<T, int>(value, priority));
+        }
+
+        public T Pop()
+        {
+            return underlying.Pop().Item1;
+        }
+
+        public T Pop(out int priority)
+        {
+            Tuple<T, int> pair = underlying.Pop();
+            priority = pair.Item2;
+            return pair.Item1;
+        }
+    }
+}
---
+++ HuffmanEncoding/Other/PriorityQueueTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoding
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PriorityQueueTest
+    {
+        [Test]
+        public void PriorityQueueTest1()
+        {
+            PriorityQueue<string> a = new PriorityQueue<string>();
+
+            a.Push("World", 7);
+            a.Push("Hello", 2);
+            Assert.AreEqual("Hello", a.Pop());
+            Assert.AreEqual("World", a.Pop());
+
+            a.Push("Hello", 2);
+            a.Push("World", 7);
+            Assert.AreEqual("Hello", a.Pop());
+            Assert.AreEqual("World", a.Pop());
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PriorityQueueTest2()
+        {
+            PriorityQueue<string> a = new PriorityQueue<string>(new[] { Tuple.Create("Hello", 4), Tuple.Create("World", 9), Tuple.Create("Test", 1) });
+            a.Push("Test2", 2);
+
+            Assert.AreEqual("Test", a.Pop());
+            int p2;
+            Assert.AreEqual("Test2", a.Pop(out p2));
+            Assert.AreEqual(2, p2);
+
+            a.Push("First", 0);
+            a.Push("Second", 5);
+            Assert.AreEqual("First", a.Pop());
+            Assert.AreEqual("Hello", a.Pop());
+            Assert.AreEqual("Second", a.Pop());
+            Assert.AreEqual("World", a.Pop());
+            a.Pop();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PriorityQueueEmpty()
+        {
+            PriorityQueue<string> a = new PriorityQueue<string>();
+            a.Pop();
+        }
+    }
+}
