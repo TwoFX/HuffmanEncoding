9704412f445a5da64509cdd1ecaecb3a821154f6
Markus Himmel <markus@himmel-villmar.de>
2015-07-11 11:05:48 +0200

    Added HuffmanTree with tests

---
+++ HuffmanEncoding/Classes/HuffmanTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoding
+{
+    abstract class HuffmanTree<TSymbol>
+    {
+        public abstract int Frequency
+        {
+            get;
+        }
+    }
+
+    class HuffmanTreeInternalNode<TSymbol> : HuffmanTree<TSymbol>
+    {
+        private HuffmanTree<TSymbol> left, right;
+
+        public override int Frequency
+        {
+            get { return left.Frequency + right.Frequency; }
+        }
+
+        public HuffmanTreeInternalNode(HuffmanTree<TSymbol> left, HuffmanTree<TSymbol> right)
+        {
+            if (left == null)
+                throw new ArgumentNullException("left");
+
+            if (right == null)
+                throw new ArgumentNullException("right");
+
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class HuffmanTreeLeaf<TSymbol> : HuffmanTree<TSymbol>
+    {
+        private TSymbol symbol;
+        private int frequency;
+
+        public override int Frequency
+        {
+            get { return frequency; }
+        }
+
+        public TSymbol Symbol
+        {
+            get { return symbol; }
+        }
+
+        public HuffmanTreeLeaf(TSymbol symbol, int frequency)
+        {
+            if (frequency < 0)
+                throw new ArgumentOutOfRangeException("frequency");
+
+            this.symbol = symbol;
+            this.frequency = frequency;
+        }
+    }
+}
---
+++ HuffmanEncoding/Tests/HuffmanTreeTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanEncoding
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class HuffmanTreeTest
+    {
+        [Test]
+        public void HuffmanTreeLeaf()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, 9);
+            Assert.AreEqual(9, a.Frequency);
+            Assert.AreEqual(5, a.Symbol);
+            
+            HuffmanTreeLeaf<int> b = new HuffmanTreeLeaf<int>(7, 21);
+            Assert.AreEqual(21, b.Frequency);
+            Assert.AreEqual(7, b.Symbol);
+        }
+
+        [Test]
+        public void HuffmanTreeInternal()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, 9);
+            HuffmanTreeLeaf<int> b = new HuffmanTreeLeaf<int>(7, 21);
+            HuffmanTreeLeaf<int> c = new HuffmanTreeLeaf<int>(2, 125);
+
+            HuffmanTreeInternalNode<int> i1 = new HuffmanTreeInternalNode<int>(a, b);
+            Assert.AreEqual(30, i1.Frequency);
+
+            HuffmanTreeInternalNode<int> i2 = new HuffmanTreeInternalNode<int>(c, i1);
+            Assert.AreEqual(155, i2.Frequency);
+        }
+
+        [Test]
+        public void HuffmanTreeCast()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, 9);
+            HuffmanTreeLeaf<int> b = new HuffmanTreeLeaf<int>(7, 21);
+            HuffmanTreeInternalNode<int> i1 = new HuffmanTreeInternalNode<int>(a, b);
+
+            HuffmanTree<int> castA = a as HuffmanTree<int>;
+            Assert.NotNull(castA);
+            Assert.AreEqual(9, castA.Frequency);
+
+            HuffmanTree<int> castI1 = i1 as HuffmanTree<int>;
+            Assert.NotNull(castI1);
+            Assert.AreEqual(30, castI1.Frequency);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HuffmanTreeNegativeFrequency()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, -1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void HuffmanTreeLeftLeafNull()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, 9);
+            HuffmanTreeInternalNode<int> i1 = new HuffmanTreeInternalNode<int>(null, a);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void HuffmanTreeRightLeafNull()
+        {
+            HuffmanTreeLeaf<int> a = new HuffmanTreeLeaf<int>(5, 9);
+            HuffmanTreeInternalNode<int> i1 = new HuffmanTreeInternalNode<int>(a, null);
+        }
+    }
+}
